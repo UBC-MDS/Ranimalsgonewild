817cc0a5dc0ff633e3fd02fed78c19f3c70b46da
Junrong Zhu <junrongz@student.ubc.ca>
2022-01-19 09:50:08 -0800

    Add animalClassifier function + Roxygen comments

---
+++ R/animalClassifier.R
@@ -0,0 +1,11 @@
+#' Classify the input text as an animal
+#'
+#' @param text String Character
+#'
+#' @return A character, the classified animal type
+#' @export
+#'
+#' @examples
+#' animalClassifier("check out this text")
+animalClassifier <- function(text){
+}

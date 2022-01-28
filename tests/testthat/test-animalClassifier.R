51177ba719a22953befc497a74305dc62c4f3b1c
Junrong Zhu <junrongz@student.ubc.ca>
2022-01-28 01:36:18 -0800

    Add test file for function 1

---
+++ tests/testthat/test-animalClassifier.R
@@ -0,0 +1,21 @@
+library(test_that)
+
+test_that("output datatype is correct", {
+  expect_equal(typeof("string"), typeof(animalClassifier("one, two three!")))
+})
+
+test_that("output value is correct", {
+  expect_equal("Duck", animalClassifier("one, one one! one"))
+})
+
+test_that("output value is correct", {
+  expect_equal("Monkey", animalClassifier("one three one! two one!"))
+})
+
+test_that("output value is correct", {
+  expect_equal("Giraffe", animalClassifier("one one! two three!"))
+})
+
+test_that("output value is correct", {
+  expect_equal("Whale", animalClassifier("one"))
+})

5fe5b981729094ee24100b457f268ca72613450c
nrao944 <nagraj944@gmail.com>
2022-01-28 11:22:52 -0800

    Fixing warning messages

--- R/wordCloud.R
+++ R/wordCloud.R
@@ -12,15 +12,19 @@ wordCloud <- function(link) {
 
   output <- NULL
   len <- NULL
+  options(warn=-1)
 
+  #CHECKING IF THE INPUT DATA IS A WEB LINK OF CHAR TYPE
   if(!is.character(link)){
     stop("Inappropriate input data type! Link should be character")
   }
 
+  #CHECKING IF THE INPUT DATA IS OF LENGTH 1
   if(length(link) != 1){
     stop("Incorrect amount of input values! Expecting only 1 input text string.")
   }
 
+  #DEPENDENCIES
   library(RCurl)
   library(XML)
   library(htm2txt)
@@ -29,23 +33,23 @@ wordCloud <- function(link) {
   library(SnowballC)
   library(wordcloud)
   library(wordcloud2)
+
+  #CONVERTING WEBLINK CONTENT INTO A TEXT FILE
   txt <- htm2txt(link)
   graphics.off()
   par(mar = rep(2, 4))
   source('http://www.sthda.com/upload/rquery_wordcloud.r')
+
+  #CREATING A DF WITH WORDS AND FREQUENCY OF OCCURENCE
   res <- rquery.wordcloud(txt, type ="file", lang = "english")$freqTable
 
+  #REMOVING PUNCTUATIONS AND SPECIAL CHARACTERS
   freqTable1 <- subset(res, grepl('^[A-Za-z]+$', word))
 
-  txt <- htm2txt(link)
-  graphics.off()
-  par(mar = rep(2, 4))
-  source('http://www.sthda.com/upload/rquery_wordcloud.r')
-  res <- rquery.wordcloud(txt, type ="file", lang = "english")$freqTable
-
-  freqTable1 <- subset(res, grepl('^[A-Za-z]+$', word))
+  #CALCULATING TOTAL NUMBER OF WORDS
   len1 <- sum(nchar(freqTable1$freq))
 
+  #SPITTING OUT WORDCLOUDS AS NAMES OF ANIMALS CONSTRUCTED FROM TEXT FROM THE WEBSITE BASED ON LENGTH
   if (len1 <= 1000) {
     letterCloud(freqTable1, word = "DUCK")
     len <- len1

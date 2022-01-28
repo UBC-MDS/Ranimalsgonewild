a6cdbd0bc3ae4bf3f639c72c16d5fd899e137a25
Junrong Zhu <junrongz@student.ubc.ca>
2022-01-28 09:31:53 -0800

    Add comments for tests

--- tests/testthat/test-animalClassifier.R
+++ tests/testthat/test-animalClassifier.R
@@ -1,9 +1,11 @@
 library(test_that)
 
+#check output datatype
 test_that("output datatype is correct", {
   expect_equal(typeof("string"), typeof(animalClassifier("one, two three!")))
 })
 
+#check output values for each animal cases
 test_that("output value is correct", {
   expect_equal("Duck", animalClassifier("one, one one! one"))
 })
